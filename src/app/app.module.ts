9ec6779ce32ffde7eefda4ac699d4d73e5da9716
martin-k15 <kostka.ma@seznam.cz>
2022-07-10 19:34:22 +0200

    start and setup the folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategoryComponent
+
   ],
   imports: [
     BrowserModule,
