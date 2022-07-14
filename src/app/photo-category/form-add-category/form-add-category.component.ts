ffb62ba2e6625b8b17f123fb0ef3c6f3fc206a97
martin-k15 <kostka.ma@seznam.cz>
2022-07-14 16:45:19 +0200

    improved design (repaired)

--- src/app/photo-category/form-add-category/form-add-category.component.ts
+++ src/app/photo-category/form-add-category/form-add-category.component.ts
@@ -19,9 +19,14 @@ export class FormAddCategoryComponent implements OnInit {
 
 
   addCategory(name: string){
+    if (name == "" || name.length > 20){
+      return false;
+    }
     this.pcService.toAddNewCategory(name);
     this.router.navigateByUrl('/categories');
+    console.log("CLos")
     //this.pcService.addToRoutes(name);
+    return true;
   }
 
 }
